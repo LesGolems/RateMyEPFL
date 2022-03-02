5f1bfa2d697e7b525576cbbfe4209b546ee9f7f4
AlexandreMessmer <alexandre.messmer@epfl.ch>
2022-03-02 22:47:19 +0100

    Add tests for MainActivity The tests were not covering the case where the user presser the Enter key. The coverage remains partial since the tests do not cover the case where the users presses another key than Enter

--- app/src/androidTest/java/com/github/sdp/ratemyepfl/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/sdp/ratemyepfl/MainActivityTest.kt
@@ -1,11 +1,15 @@
 package com.github.sdp.ratemyepfl
 
+import android.view.KeyEvent
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.*
 import androidx.test.espresso.intent.matcher.IntentMatchers.*
@@ -38,13 +42,36 @@ class MainActivityTest {
     }
 
     @Test
-    fun firesAnIntentWhenTheUsersPressesButton() {
+    fun firesAnIntentWhenUserPressesButton() {
         Intents.init()
         val name = "my name"
         onView(withId(R.id.mainName)).perform(typeText(name))
         onView(withId(R.id.mainGoButton)).perform(click())
 
         intended(toPackage("com.github.sdp.ratemyepfl"))
+        release()
+    }
+
+    @Test
+    fun firesAnIntentWhenUserPressesEnter() {
+        Intents.init()
+        val name = "John"
+        onView(withId(R.id.mainName)).perform(typeText(name))
+            .perform(ViewActions.pressImeActionButton())
+
+        intended(toPackage("com.github.sdp.ratemyepfl"))
+        intended(hasExtra(GreetingActivity.EXTRA_USER_NAME, name))
+        release()
+    }
+
+    @Test
+    fun doesNotFireAnIntentWithOtherKeyThanEnter() {
+        Intents.init()
+        val name = "John"
+        onView(withId(R.id.mainName)).perform(typeText(name))
+            .perform(ViewActions.pressKey(KeyEvent.KEYCODE_D))
+
+        // How can we check that no intent is fired ?
         release()
     }
 
