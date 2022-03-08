76a463454298313c094207851a4560f1e09880f2
bouillou <mathias.bouilloud@epfl.ch>
2022-03-08 11:45:01 +0100

    Added hilt to project, had to change build.gradle files

--- app/src/main/java/com/github/sdp/ratemyepfl/MainActivity.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/MainActivity.kt
@@ -3,14 +3,15 @@ package com.github.sdp.ratemyepfl
 import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.sdp.ratemyepfl.auth.UserAuth
 import com.github.sdp.ratemyepfl.auth.UserAuthImpl
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var mLoginButton: Button
     private lateinit var mLogoutButton: Button
