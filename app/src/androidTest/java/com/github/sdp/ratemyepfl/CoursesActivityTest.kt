c3ac78dc5795a28e3fd4c6081c4658c41d338c90
Nicolas Matekalo <nicolas.matekalo@epfl.ch>
2022-03-11 00:20:18 +0100

    Fix tests: launch correctly activities

--- app/src/androidTest/java/com/github/sdp/ratemyepfl/CoursesActivityTest.kt
+++ app/src/androidTest/java/com/github/sdp/ratemyepfl/CoursesActivityTest.kt
@@ -7,9 +7,19 @@ import androidx.test.espresso.intent.Intents.*
 import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import dagger.hilt.android.testing.HiltAndroidRule
+import org.junit.Rule
 import org.junit.Test
 
 class CoursesActivityTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val testRule = ActivityScenarioRule(CoursesActivity::class.java)
+
     @Test
     fun isCoursesListViewViewVisibleOnActivityLaunch() {
         onView(withId(R.id.coursesListView))
--- app/src/androidTest/java/com/github/sdp/ratemyepfl/ReviewsActivityTest.kt
+++ app/src/androidTest/java/com/github/sdp/ratemyepfl/ReviewsActivityTest.kt
@@ -4,9 +4,19 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import dagger.hilt.android.testing.HiltAndroidRule
+import org.junit.Rule
 import org.junit.Test
 
 class ReviewsActivityTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val testRule = ActivityScenarioRule(ReviewsActivity::class.java)
+
     @Test
     fun isCoursesListViewViewVisibleOnActivityLaunch() {
         onView(withId(R.id.reviewsListView))
