e314fc7c3835e6882a2ae6c39d2500a90f206fcf
Nicolas Matekalo <nicolas.matekalo@epfl.ch>
2022-03-10 16:21:55 +0100

    Create activity for reading reviews of a course

--- app/src/main/java/com/github/sdp/ratemyepfl/ReviewsActivity.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/ReviewsActivity.kt
@@ -8,22 +8,22 @@ import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.sdp.ratemyepfl.item.FakeCoursesDatabase
 
-class CoursesActivity : AppCompatActivity() {
-    private lateinit var coursesView: ListView
+class ReviewsActivity : AppCompatActivity() {
+    private lateinit var reviewsView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_courses)
+        setContentView(R.layout.activity_reviews)
 
         val fakeDatabase = FakeCoursesDatabase()
         val courseList = fakeDatabase.getCoursesList()
 
-        coursesView = findViewById(R.id.coursesListView)
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
+        reviewsView = findViewById(R.id.coursesListView)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_2,
             courseList)
-        coursesView.adapter = adapter
-        coursesView.isClickable = true
-        coursesView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+        reviewsView.adapter = adapter
+        reviewsView.isClickable = true
+        reviewsView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
             printCourse(courseList[position].name)
         }
     }
