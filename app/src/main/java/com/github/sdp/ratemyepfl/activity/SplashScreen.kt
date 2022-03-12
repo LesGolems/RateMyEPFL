f68e00a2403db245dfd535fef5916275bcb10384
bouillou <mathias.bouilloud@epfl.ch>
2022-03-12 23:26:25 +0100

    Create SplashScreen activity, with background and google button

---
+++ app/src/main/java/com/github/sdp/ratemyepfl/activity/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.github.sdp.ratemyepfl.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.sdp.ratemyepfl.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
