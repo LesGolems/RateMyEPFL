7e6743459f6fcb96bd93c987a902bef98ce51bbc
jmettler27 <julien.mettler@epfl.ch>
2022-03-13 20:25:57 +0100

    Implement the search feature for courses

--- app/src/main/java/com/github/sdp/ratemyepfl/activity/CoursesActivity.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/activity/CoursesActivity.kt
@@ -2,16 +2,20 @@ package com.github.sdp.ratemyepfl.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.github.sdp.ratemyepfl.R
+import com.github.sdp.ratemyepfl.model.items.Course
 import com.github.sdp.ratemyepfl.model.items.FakeCoursesDatabase
 
 class CoursesActivity : AppCompatActivity() {
 
     private lateinit var coursesView: ListView
+    private lateinit var adapter: ArrayAdapter<Course>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +25,10 @@ class CoursesActivity : AppCompatActivity() {
         val courseList = fakeDatabase.getCoursesList()
 
         coursesView = findViewById(R.id.coursesListView)
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
-            courseList)
+        adapter = ArrayAdapter(
+            this, android.R.layout.simple_list_item_1,
+            courseList
+        )
         coursesView.adapter = adapter
         coursesView.isClickable = true
         coursesView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
@@ -34,5 +40,24 @@ class CoursesActivity : AppCompatActivity() {
         val intent = Intent(this, ReviewsActivity::class.java)
         intent.putExtra(ReviewsActivity.EXTRA_COURSE_NAME, courseName)
         startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+        val menuItem = menu!!.findItem(R.id.searchView)
+        val searchView = menuItem.actionView as SearchView
+        searchView.maxWidth = Int.MAX_VALUE
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filter.filter(newText)
+                return true
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
