7b48387f862f1bdeff8236310c37a75586b1318b
Nicolas Matekalo <nicolas.matekalo@epfl.ch>
2022-03-24 23:08:38 +0100

    Add other tests for MapActivity

--- app/src/androidTest/java/com/github/sdp/ratemyepfl/activity/MapActivityTest.kt
+++ app/src/androidTest/java/com/github/sdp/ratemyepfl/activity/MapActivityTest.kt
@@ -1,10 +1,19 @@
 package com.github.sdp.ratemyepfl.activity
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.github.sdp.ratemyepfl.R
 import com.github.sdp.ratemyepfl.activity.map.MapActivity
+import com.github.sdp.ratemyepfl.utils.withMenuItemText
+import com.github.sdp.ratemyepfl.utils.withOverflowMenuItemText
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
+import org.junit.Test
 
 @HiltAndroidTest
 class MapActivityTest {
@@ -15,5 +24,41 @@ class MapActivityTest {
     @get:Rule(order = 1)
     val testRule = ActivityScenarioRule(MapActivity::class.java)
 
+    @Test
+    fun isMapVisibleOnActivityLaunch() {
+        onView(withId(R.id.map))
+            .check(ViewAssertions.matches(isDisplayed()))
+    }
 
+    @Test
+    fun changesTypeCorrectlyWhenUserClicksOnStandard() {
+        onView(withOverflowMenuItemText(R.string.change_map_type)).perform(click())
+        onView(withMenuItemText(R.string.standard_type)).perform(click())
+        onView(withId(R.id.map))
+            .check(ViewAssertions.matches(isDisplayed()))
+    }
+
+    @Test
+    fun changesTypeCorrectlyWhenUserClicksOnSatellite() {
+        onView(withOverflowMenuItemText(R.string.change_map_type)).perform(click())
+        onView(withMenuItemText(R.string.satellite_type)).perform(click())
+        onView(withId(R.id.map))
+            .check(ViewAssertions.matches(isDisplayed()))
+    }
+
+    @Test
+    fun changesTypeCorrectlyWhenUserClicksOnHybrid() {
+        onView(withOverflowMenuItemText(R.string.change_map_type)).perform(click())
+        onView(withMenuItemText(R.string.hybrid_type)).perform(click())
+        onView(withId(R.id.map))
+            .check(ViewAssertions.matches(isDisplayed()))
+    }
+
+    @Test
+    fun changesTypeCorrectlyWhenUserClicksOnTerrain() {
+        onView(withOverflowMenuItemText(R.string.change_map_type)).perform(click())
+        onView(withMenuItemText(R.string.terrain_type)).perform(click())
+        onView(withId(R.id.map))
+            .check(ViewAssertions.matches(isDisplayed()))
+    }
 }
