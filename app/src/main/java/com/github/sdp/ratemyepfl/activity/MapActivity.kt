e18db527df47e316d8d4c1af6fd8927e52e8d40a
Nicolas Matekalo <nicolas.matekalo@epfl.ch>
2022-03-24 17:10:39 +0100

    Add map menu to choose map type

--- app/src/main/java/com/github/sdp/ratemyepfl/activity/MapActivity.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/activity/MapActivity.kt
@@ -1,6 +1,8 @@
 package com.github.sdp.ratemyepfl.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.github.sdp.ratemyepfl.R
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -11,15 +13,19 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mapFragment: SupportMapFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
         // Get the SupportMapFragment and request notification when the map is ready to be used.
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -30,5 +36,32 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                 .title("Marker at EPFL")
         )
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(epfl))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.map_options_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.standardType -> {
+            mapFragment.getMapAsync { g -> g.mapType = GoogleMap.MAP_TYPE_NORMAL }
+            true
+        }
+        R.id.satelliteType -> {
+            mapFragment.getMapAsync { g -> g.mapType = GoogleMap.MAP_TYPE_SATELLITE }
+            true
+        }
+        R.id.hybridType -> {
+            mapFragment.getMapAsync { g -> g.mapType = GoogleMap.MAP_TYPE_HYBRID }
+            true
+        }
+        R.id.terrainType -> {
+            mapFragment.getMapAsync { g -> g.mapType = GoogleMap.MAP_TYPE_TERRAIN }
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
