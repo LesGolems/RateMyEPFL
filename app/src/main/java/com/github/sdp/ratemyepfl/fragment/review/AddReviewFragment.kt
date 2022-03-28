e52e683d814ac00482c802d1af7af0ee0b363765
bouillou <mathias.bouilloud@epfl.ch>
2022-03-29 00:49:22 +0200

    imports clean

--- app/src/main/java/com/github/sdp/ratemyepfl/fragment/review/AddReviewFragment.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/fragment/review/AddReviewFragment.kt
@@ -1,7 +1,5 @@
 package com.github.sdp.ratemyepfl.fragment.review
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -9,20 +7,15 @@ import android.view.View
 import android.widget.Button
 import android.widget.RatingBar
 import android.widget.TextView
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.github.sdp.ratemyepfl.R
 import com.github.sdp.ratemyepfl.model.review.ReviewRating
 import com.github.sdp.ratemyepfl.viewmodel.AddReviewViewModel
 import com.github.sdp.ratemyepfl.viewmodel.ReviewViewModel
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
