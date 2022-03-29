9f33cfd7b205b109ab6affe280143376a82dec79
bouillou <mathias.bouilloud@epfl.ch>
2022-03-29 18:55:50 +0200

    remove import

--- app/src/main/java/com/github/sdp/ratemyepfl/database/Repository.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/database/Repository.kt
@@ -4,12 +4,11 @@ import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
-import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.tasks.await
 
 open class Repository {
     protected val db = FirebaseFirestore.getInstance()
-    
+
     suspend fun getLimit(collection: CollectionReference, limit: Long): QuerySnapshot {
         return collection.limit(limit).get().await()
     }
