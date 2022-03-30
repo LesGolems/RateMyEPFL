1324d42c8215a0bc16a1f54332bf910aa75ea5a5
AlexandreMessmer <alexandre.messmer@epfl.ch>
2022-03-30 21:32:47 +0200

    Suppress deprecated test

--- app/src/androidTest/java/com/github/sdp/ratemyepfl/database/CourseRepositoryTest.kt
+++ app/src/androidTest/java/com/github/sdp/ratemyepfl/database/CourseRepositoryTest.kt
@@ -14,7 +14,6 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
-import org.mockito.junit.MockitoJUnitRunner
 import javax.inject.Inject
 
 @HiltAndroidTest
