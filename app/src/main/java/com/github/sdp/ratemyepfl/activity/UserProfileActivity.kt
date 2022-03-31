993e78b2812fdc39d2e6e91ce81a97ce721ffa7b
Sou-ly <souleyman.boudouh@epfl.ch>
2022-03-31 16:51:17 +0200

    Can upload and download picture

--- app/src/main/java/com/github/sdp/ratemyepfl/activity/UserProfileActivity.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/activity/UserProfileActivity.kt
@@ -1,6 +1,8 @@
 package com.github.sdp.ratemyepfl.activity
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -9,11 +11,14 @@ import android.view.View
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import com.github.sdp.ratemyepfl.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import de.hdodenhof.circleimageview.CircleImageView
+import java.io.ByteArrayOutputStream
 
 class UserProfileActivity : AppCompatActivity() {
 
-    val SELECT_IMAGE = 123
+    val SELECT_IMAGE = 1044
 
     private lateinit var profilePicture: CircleImageView
     private lateinit var cameraIcon: CircleImageView
@@ -24,8 +29,28 @@ class UserProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_user_profile)
 
         profilePicture = findViewById(R.id.profile_image)
+
+        val storageRef = FirebaseStorage.getInstance().reference
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            storageRef.child("images/user_pictures/")
+                .child(currentUser.uid + ".jpg")
+                .getBytes(1024*1024)
+                .addOnSuccessListener {
+                    it?.let {
+                        val bitmap = BitmapFactory.decodeByteArray(it, 0, it.size)
+                        profilePicture.setImageBitmap(bitmap)
+                    }
+                }
+                .addOnFailureListener{
+                    it.printStackTrace()
+                }
+        }
+
         cameraIcon = findViewById(R.id.modify_profile_image_button)
         emailTextView = findViewById(R.id.emailText)
+
+        emailTextView.setText(FirebaseAuth.getInstance().currentUser?.displayName)
 
         cameraIcon.setOnClickListener(updatePicture)
     }
@@ -45,9 +70,25 @@ class UserProfileActivity : AppCompatActivity() {
             SELECT_IMAGE -> {
                 if (resultCode == RESULT_OK && data != null) {
                     val photoUri = data.data
-                        val source = ImageDecoder.createSource(this.contentResolver, photoUri!!)
-                        val bitmap = ImageDecoder.decodeBitmap(source)
-                        profilePicture.setImageBitmap(bitmap)
+                    val source = ImageDecoder.createSource(this.contentResolver, photoUri!!)
+                    val bitmap = ImageDecoder.decodeBitmap(source)
+                    val currentUser = FirebaseAuth.getInstance().currentUser
+
+                    if (currentUser != null) {
+                        val storageReference = FirebaseStorage.getInstance().reference
+                        val baos = ByteArrayOutputStream()
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                        storageReference.child("images/")
+                            .child("user_pictures/")
+                            .child(currentUser.uid + ".jpg")
+                            .putBytes(baos.toByteArray())
+                            .addOnSuccessListener {
+                                profilePicture.setImageBitmap(bitmap)
+                            }
+                            .addOnFailureListener{
+                                it.printStackTrace()
+                            }
+                    }
                 }
             }
         }
