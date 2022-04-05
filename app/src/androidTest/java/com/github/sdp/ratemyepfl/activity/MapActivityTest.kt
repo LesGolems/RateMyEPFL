c93b876d0aafde3eb1cfe4e565090838917b8ca7
bouillou <mathias.bouilloud@epfl.ch>
2022-04-05 21:52:15 +0200

    add location rule

--- app/src/androidTest/java/com/github/sdp/ratemyepfl/activity/MapActivityTest.kt
+++ app/src/androidTest/java/com/github/sdp/ratemyepfl/activity/MapActivityTest.kt
@@ -1,10 +1,21 @@
 package com.github.sdp.ratemyepfl.activity
 
+import android.Manifest
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.rule.GrantPermissionRule
+import com.github.sdp.ratemyepfl.R
 import com.github.sdp.ratemyepfl.activity.map.MapActivity
+import com.github.sdp.ratemyepfl.utils.withMenuItemText
+import com.github.sdp.ratemyepfl.utils.withOverflowMenuItemText
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
+import org.junit.Test
 
 @HiltAndroidTest
 class MapActivityTest {
@@ -15,11 +26,14 @@ class MapActivityTest {
     @get:Rule(order = 1)
     val testRule = ActivityScenarioRule(MapActivity::class.java)
 
-    /*
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule =
+        GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION)
+
     @Test
     fun isMapVisibleOnActivityLaunch() {
         onView(withId(R.id.map))
-            .check(ViewAssertions.matches(isDisplayed()))
+            .check(matches(isDisplayed()))
     }
 
     @Test
@@ -27,7 +41,7 @@ class MapActivityTest {
         onView(withOverflowMenuItemText(R.string.change_map_type)).perform(click())
         onView(withMenuItemText(R.string.standard_type)).perform(click())
         onView(withId(R.id.map))
-            .check(ViewAssertions.matches(isDisplayed()))
+            .check(matches(isDisplayed()))
     }
 
     @Test
@@ -35,7 +49,7 @@ class MapActivityTest {
         onView(withOverflowMenuItemText(R.string.change_map_type)).perform(click())
         onView(withMenuItemText(R.string.satellite_type)).perform(click())
         onView(withId(R.id.map))
-            .check(ViewAssertions.matches(isDisplayed()))
+            .check(matches(isDisplayed()))
     }
 
     @Test
@@ -43,7 +57,7 @@ class MapActivityTest {
         onView(withOverflowMenuItemText(R.string.change_map_type)).perform(click())
         onView(withMenuItemText(R.string.hybrid_type)).perform(click())
         onView(withId(R.id.map))
-            .check(ViewAssertions.matches(isDisplayed()))
+            .check(matches(isDisplayed()))
     }
 
     @Test
@@ -51,7 +65,6 @@ class MapActivityTest {
         onView(withOverflowMenuItemText(R.string.change_map_type)).perform(click())
         onView(withMenuItemText(R.string.terrain_type)).perform(click())
         onView(withId(R.id.map))
-            .check(ViewAssertions.matches(isDisplayed()))
+            .check(matches(isDisplayed()))
     }
-     */
 }
