5d0e8174202ca662d6cec3cc4f37204002a74646
Nicolas Matekalo <nicolas.matekalo@epfl.ch>
2022-04-23 17:57:02 +0200

    Test EventItem and fix bugs

--- app/src/main/java/com/github/sdp/ratemyepfl/model/items/EventItem.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/model/items/EventItem.kt
@@ -2,6 +2,8 @@ package com.github.sdp.ratemyepfl.model.items
 
 import android.content.Intent
 import androidx.fragment.app.FragmentActivity
+import com.github.sdp.ratemyepfl.R
+import com.github.sdp.ratemyepfl.activity.ReviewActivity
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.LatLng
 
@@ -9,6 +11,9 @@ class EventItem(event: Event, photo: Int, icon: BitmapDescriptor?) :
     MapItem(LatLng(event.lat, event.long), event.id, photo, icon) {
 
     override fun onClickIntent(activity: FragmentActivity?): Intent {
-        TODO("Not yet implemented")
+        val intent = Intent(activity, ReviewActivity::class.java)
+        intent.putExtra(ReviewActivity.EXTRA_ITEM_REVIEWED, name)
+        intent.putExtra(ReviewActivity.EXTRA_LAYOUT_ID, R.layout.activity_restaurant_review) // Will have to change later
+        return intent
     }
 }
