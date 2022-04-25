250a95a4db4cf738e8225880dbb10c8517c2506f
bouillou <mathias.bouilloud@epfl.ch>
2022-04-25 16:09:19 +0200

    scroll view test

--- app/src/androidTest/java/com/github/sdp/ratemyepfl/fragment/review/AddCourseReviewFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp/ratemyepfl/fragment/review/AddCourseReviewFragmentTest.kt
@@ -5,8 +5,7 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
