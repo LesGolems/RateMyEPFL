7d54b15f9adcc533269103d71ff18361f1274e92
Nicolas Matekalo <nicolas.matekalo@epfl.ch>
2022-04-28 22:02:39 +0200

    Fill AddEventReviewFragment

--- app/src/main/java/com/github/sdp/ratemyepfl/fragment/review/AddEventReviewFragment.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/fragment/review/AddEventReviewFragment.kt
@@ -1,4 +1,29 @@
 package com.github.sdp.ratemyepfl.fragment.review
 
-class AddEventReviewFragment {
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
+import com.github.sdp.ratemyepfl.R
+import com.github.sdp.ratemyepfl.viewmodel.EventInfoViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AddEventReviewFragment : AddReviewFragment() {
+    private lateinit var reviewIndicationTitle: TextView
+
+    private val eventReviewViewModel: EventInfoViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        reviewIndicationTitle = view.findViewById(R.id.reviewTitle)
+
+        reviewIndicationTitle.text = getString(R.string.title_review, eventReviewViewModel.id)
+    }
+
+    override fun submitReview(): Boolean {
+        val rating = addReviewViewModel.submitReview(eventReviewViewModel.id) ?: return false
+        eventReviewViewModel.updateRating(rating)
+        return true
+    }
 }
