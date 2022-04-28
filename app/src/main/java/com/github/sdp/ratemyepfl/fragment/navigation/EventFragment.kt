2baf33122d94c97ad4bab758ece1ec08b1a2fee0
Nicolas Matekalo <nicolas.matekalo@epfl.ch>
2022-04-28 17:56:27 +0200

    Instanciate fragments for Event

--- app/src/main/java/com/github/sdp/ratemyepfl/fragment/navigation/EventFragment.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/fragment/navigation/EventFragment.kt
@@ -1,6 +1,28 @@
 package com.github.sdp.ratemyepfl.fragment.navigation
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.github.sdp.ratemyepfl.R
+import com.github.sdp.ratemyepfl.viewmodel.EventListViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class EventFragment : Fragment(R.layout.fragment_event)
+@AndroidEntryPoint
+class EventFragment : ReviewableTabFragment() {
+    private val viewModel: EventListViewModel by viewModels()
+
+    override val reviewActivityLayoutId: Int = R.layout.activity_event_review
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.events
+            .observe(viewLifecycleOwner) { events ->
+                reviewableAdapter.submitData(events)
+            }
+    }
+
+    override fun onResume() {
+        viewModel.events.postValue(viewModel.events.value ?: listOf())
+        super.onResume()
+    }
+}
