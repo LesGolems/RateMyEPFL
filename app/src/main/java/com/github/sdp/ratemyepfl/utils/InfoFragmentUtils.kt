87505d321799cb64fae988f611a499335e7432e1
bouillou <mathias.bouilloud@epfl.ch>
2022-04-28 16:07:07 +0200

    doc

--- app/src/main/java/com/github/sdp/ratemyepfl/utils/InfoFragmentUtils.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/utils/InfoFragmentUtils.kt
@@ -5,6 +5,12 @@ import com.github.sdp.ratemyepfl.R
 
 object InfoFragmentUtils {
 
+    /**
+     * Returns the string, that will be displayed, describing the number of reviews for
+     * a reviewable item
+     * @param context : context of the activity/fragment
+     * @param numReview : number of reviews for the reviewable item
+     */
     fun getNumReviewString(context: Context, numReview: Int): String {
         return if (numReview == 0) {
             context.getString(R.string.zero_num_reviews)
