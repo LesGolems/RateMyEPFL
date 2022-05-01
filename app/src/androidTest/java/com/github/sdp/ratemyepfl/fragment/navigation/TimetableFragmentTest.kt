1bb8b4429853f38d0c780a7e2c66031730aae022
SuperSpeedWagon <emilien.guandalino@epfl.ch>
2022-05-01 17:41:32 +0200

    Class tests

---
+++ app/src/androidTest/java/com/github/sdp/ratemyepfl/fragment/navigation/TimetableFragmentTest.kt
@@ -0,0 +1,14 @@
+package com.github.sdp.ratemyepfl.fragment.navigation
+
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+
+@HiltAndroidTest
+class TimetableFragmentTest {
+    @get:Rule
+    val hiltAndroidRule: HiltAndroidRule = HiltAndroidRule(this)
+
+
+
+}
