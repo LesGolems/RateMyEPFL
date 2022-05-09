00622aa419e23ac9ece2063227b819cea9827a44
Julien Mettler <julien.mettler@epfl.ch>
2022-05-09 22:31:52 +0200

    Move the initialization of recorder and player to onCreate

--- app/src/main/java/com/github/sdp/ratemyepfl/activity/AudioRecordTest.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/activity/AudioRecordTest.kt
@@ -1,17 +1,14 @@
 package com.github.sdp.ratemyepfl.activity
 
-import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import android.media.MediaRecorder
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.sdp.ratemyepfl.R
 import kotlin.math.log10
-
-private const val LOG_TAG = "AudioRecordTest"
 
 open class AudioRecordTest : AppCompatActivity() {
 
@@ -56,6 +53,13 @@ open class AudioRecordTest : AppCompatActivity() {
             mStartPlaying = !mStartPlaying
         }
 
+        recorder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            MediaRecorder(this)
+        } else {
+            MediaRecorder()
+        }
+
+        player = MediaPlayer()
     }
 
     override fun onStop() {
@@ -83,24 +87,19 @@ open class AudioRecordTest : AppCompatActivity() {
         return 20 * log10(maxAmplitude / referenceAmplitude)
     }
 
-    @SuppressLint("NewApi")
     private fun startRecording() {
-        recorder = MediaRecorder(this)
-
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC)
         recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
         recorder.setOutputFile(filename)
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
         recorder.prepare()
         recorder.start() // Recording is now started
-        Log.d(LOG_TAG, "Start recording OK")
     }
 
     private fun stopRecording() {
         recorder.reset() // You can reuse the object by going back to setAudioSource() step
         recorder.release() // Now the object cannot be reused
     }
-
 
     private fun onPlay(start: Boolean) = if (start) {
         startPlaying()
@@ -109,8 +108,6 @@ open class AudioRecordTest : AppCompatActivity() {
     }
 
     private fun startPlaying() {
-        player = MediaPlayer()
-
         player.setDataSource(filename)
         player.prepare()
         player.start()
