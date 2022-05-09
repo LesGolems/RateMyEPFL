98486937e9535e6c550b3e394cb59d69e161b8d7
Julien Mettler <julien.mettler@epfl.ch>
2022-05-09 21:25:21 +0200

    Add basic record activity

---
+++ app/src/main/java/com/github/sdp/ratemyepfl/activity/AudioRecordTest.kt
@@ -0,0 +1,123 @@
+package com.github.sdp.ratemyepfl.activity
+
+import android.annotation.SuppressLint
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.github.sdp.ratemyepfl.R
+import kotlin.math.log10
+
+private const val LOG_TAG = "AudioRecordTest"
+
+open class AudioRecordTest : AppCompatActivity() {
+
+    private lateinit var filename: String
+
+    private lateinit var audioRecordButton: Button
+    private lateinit var recorder: MediaRecorder
+    private var mStartRecording = true
+
+    private lateinit var audioPlayButton: Button
+    private lateinit var player: MediaPlayer
+    private var mStartPlaying = true
+
+    companion object {
+        private const val referenceAmplitude = 0.00001
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio_record)
+
+        // Record to the external cache directory for visibility
+        filename = "${externalCacheDir!!.absolutePath}/audiorecordtest.3gp"
+
+        audioRecordButton = findViewById(R.id.audioRecordButton)
+        audioRecordButton.setOnClickListener {
+            onRecord(mStartRecording)
+            audioRecordButton.text = when (mStartRecording) {
+                true -> "Stop recording"
+                false -> "Start recording"
+            }
+            mStartRecording = !mStartRecording
+        }
+
+        audioPlayButton = findViewById(R.id.audioPlayButton)
+        audioPlayButton.setOnClickListener {
+            onPlay(mStartPlaying)
+            audioPlayButton.text = when (mStartPlaying) {
+                true -> "Stop playing"
+                false -> "Start playing"
+            }
+            mStartPlaying = !mStartPlaying
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        recorder.release()
+        player.release()
+    }
+
+    private fun onRecord(start: Boolean) {
+        if (start) {
+            startRecording()
+            for (i in 1..5) {
+                val decibels = getAmplitude()
+                Toast.makeText(this, "$decibels dB", Toast.LENGTH_SHORT).show()
+                Thread.sleep(1000)
+            }
+
+        } else {
+            stopRecording()
+        }
+    }
+
+    private fun getAmplitude(): Double {
+        val maxAmplitude = recorder.maxAmplitude.toDouble()
+        return 20 * log10(maxAmplitude / referenceAmplitude)
+    }
+
+    @SuppressLint("NewApi")
+    private fun startRecording() {
+        recorder = MediaRecorder(this)
+
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        recorder.setOutputFile(filename)
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        recorder.prepare()
+        recorder.start() // Recording is now started
+        Log.d(LOG_TAG, "Start recording OK")
+    }
+
+    private fun stopRecording() {
+        recorder.reset() // You can reuse the object by going back to setAudioSource() step
+        recorder.release() // Now the object cannot be reused
+    }
+
+
+    private fun onPlay(start: Boolean) = if (start) {
+        startPlaying()
+    } else {
+        stopPlaying()
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer()
+
+        player.setDataSource(filename)
+        player.prepare()
+        player.start()
+    }
+
+    private fun stopPlaying() {
+        player.release()
+    }
+
+}
--- app/src/main/java/com/github/sdp/ratemyepfl/fragment/review/RoomReviewInfoFragment.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/fragment/review/RoomReviewInfoFragment.kt
@@ -1,13 +1,18 @@
 package com.github.sdp.ratemyepfl.fragment.review
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.github.sdp.ratemyepfl.R
+import com.github.sdp.ratemyepfl.activity.AudioRecordTest
 import com.github.sdp.ratemyepfl.utils.InfoFragmentUtils.getNumReviewString
+import com.github.sdp.ratemyepfl.utils.PermissionUtils
 import com.github.sdp.ratemyepfl.viewmodel.ClassroomInfoViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,18 +25,40 @@ class RoomReviewInfoFragment : Fragment(R.layout.fragment_room_review_info) {
     // Gets the shared view model
     private val viewModel by activityViewModels<ClassroomInfoViewModel>()
 
+    private lateinit var noiseMeasureButton: ImageButton
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.room.observe(viewLifecycleOwner) {
             view.findViewById<TextView>(R.id.roomIdInfo).text = it?.toString()
         }
-        viewModel.numReviews.observe(viewLifecycleOwner){
+        viewModel.numReviews.observe(viewLifecycleOwner) {
             view.findViewById<TextView>(R.id.roomNumReview).text =
                 getNumReviewString(requireContext(), it)
         }
-        viewModel.averageGrade.observe(viewLifecycleOwner){
+        viewModel.averageGrade.observe(viewLifecycleOwner) {
             view.findViewById<RatingBar>(R.id.roomRatingBar).rating = it.toFloat()
         }
+
+        // Record audio
+        val audioPermissionLauncher =
+            PermissionUtils.requestPermissionLauncher({ startAudio() }, this, requireContext())
+
+        noiseMeasureButton = view.findViewById(R.id.noiseMeasureButton)
+        noiseMeasureButton.setOnClickListener {
+            PermissionUtils.startPhoneFeature(
+                { startAudio() },
+                audioPermissionLauncher,
+                requireContext(),
+                Manifest.permission.RECORD_AUDIO
+            )
+        }
+
+    }
+
+    fun startAudio() {
+        val intent = Intent(requireContext(), AudioRecordTest::class.java)
+        startActivity(intent)
     }
 
     override fun onResume() {
