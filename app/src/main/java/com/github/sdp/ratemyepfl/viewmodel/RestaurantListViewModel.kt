de44898d478def86aecca7714f10d9afc91bdb2e
bouillou <mathias.bouilloud@epfl.ch>
2022-05-12 14:40:18 +0200

    refactor occupancy code to service

--- app/src/main/java/com/github/sdp/ratemyepfl/viewmodel/RestaurantListViewModel.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/viewmodel/RestaurantListViewModel.kt
@@ -1,7 +1,5 @@
 package com.github.sdp.ratemyepfl.viewmodel
 
-import android.location.Location
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -10,16 +8,10 @@ import com.github.sdp.ratemyepfl.model.items.Restaurant
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.math.abs
 
 @HiltViewModel
 class RestaurantListViewModel @Inject constructor(private val repository: RestaurantRepository) :
     ViewModel() {
-
-    private val precision: Double = 0.0001
-
-    private val nearbyRestaurants: List<Restaurant> = listOf()
-    private var insideRestaurantId: String? = null
 
     private var items = MutableLiveData<List<Restaurant>>()
     val restaurants = MutableLiveData<List<Restaurant>>()
@@ -29,45 +21,4 @@ class RestaurantListViewModel @Inject constructor(private val repository: Restau
             restaurants.value = repository.getRestaurants()
         }
     }
-
-    fun getItemsAsLiveData(): LiveData<List<Restaurant>> {
-        return items
-    }
-
-
-    fun getRestaurantByLocation(location: Location): LiveData<List<Restaurant>> {
-        TODO()
-    }
-
-    fun updateRestaurantsOccupancy(l: Location) {
-        viewModelScope.launch {
-            val id = findCloseRestaurantID(l)
-            if (id != insideRestaurantId) {
-                insideRestaurantId = if (id == null) {
-                    repository.decrementOccupancy(insideRestaurantId!!)
-                    null
-                } else {
-                    repository.incrementOccupancy(id)
-                    if (insideRestaurantId != null) {
-                        repository.decrementOccupancy(insideRestaurantId!!)
-                    }
-                    id
-                }
-            }
-        }
-    }
-
-    private suspend fun findCloseRestaurantID(l: Location): String? {
-        repository.getRestaurants().map { r ->
-            if (isClose(l.latitude, r.lat, l.longitude, r.long)) {
-                return r.name
-            }
-        }
-        return null
-    }
-
-    private fun isClose(lat1: Double, lat2: Double, long1: Double, long2: Double): Boolean {
-        return (abs(lat1 - lat2) < precision) && (abs(long1 - long2) < precision)
-    }
-
 }
