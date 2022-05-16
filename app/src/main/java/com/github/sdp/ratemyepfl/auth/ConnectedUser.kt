47f138bf1ca925a1cce03691f27f22ad244e452f
bouillou <mathias.bouilloud@epfl.ch>
2022-05-16 11:10:33 +0200

    fix update issue

--- app/src/main/java/com/github/sdp/ratemyepfl/auth/ConnectedUser.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/auth/ConnectedUser.kt
@@ -1,5 +1,7 @@
 package com.github.sdp.ratemyepfl.auth
 
+import com.google.firebase.auth.FirebaseAuth
+
 /*
 Interface providing information about the current user
  */
@@ -24,4 +26,6 @@ interface ConnectedUser {
     fun getEmail(): String?
 
     fun getUsername(): String?
+
+    fun addCallBack(c: (FirebaseAuth) -> Unit)
 }
--- app/src/main/java/com/github/sdp/ratemyepfl/viewmodel/profile/UserViewModel.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/viewmodel/profile/UserViewModel.kt
@@ -34,6 +34,9 @@ class UserViewModel @Inject constructor(
 
     init {
         refreshUser()
+        currentUser.addCallBack {
+            refreshUser()
+        }
     }
 
     /**
