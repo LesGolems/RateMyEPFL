3a30062618a428daf1769437cf2d6a52bdffc860
Nicolas Matekalo <nicolas.matekalo@epfl.ch>
2022-05-23 12:45:20 +0200

    Try a fix for the same tests

--- app/src/androidTest/java/com/github/sdp/ratemyepfl/fragment/review/RoomReviewPictureFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp/ratemyepfl/fragment/review/RoomReviewPictureFragmentTest.kt
@@ -6,6 +6,7 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.intent.Intents.*
@@ -19,7 +20,6 @@ import com.github.sdp.ratemyepfl.adapter.RoomPictureAdapter
 import com.github.sdp.ratemyepfl.database.fakes.FakeImageStorage
 import com.github.sdp.ratemyepfl.model.items.Classroom
 import com.github.sdp.ratemyepfl.model.serializer.putExtra
-import com.github.sdp.ratemyepfl.utils.CustomViewActions.ViewPagerAction
 import com.github.sdp.ratemyepfl.utils.TestUtils.createImageGallerySetResultStub
 import com.github.sdp.ratemyepfl.utils.TestUtils.getActivity
 import com.github.sdp.ratemyepfl.utils.TestUtils.savePickedImage
@@ -53,9 +53,9 @@ class RoomReviewPictureFragmentTest {
         intent.putExtra(ReviewActivity.EXTRA_ITEM_REVIEWED_ID, "Fake id")
         scenario = ActivityScenario.launch(intent)
         Thread.sleep(500)
-        ViewPagerAction.swipeNext()
+        onView(withId(R.id.roomInfoImage)).perform(swipeLeft())
         Thread.sleep(500)
-        ViewPagerAction.swipeNext()
+        onView(withId(R.id.reviewRecyclerView)).perform(swipeLeft())
         Thread.sleep(1000)
     }
 
