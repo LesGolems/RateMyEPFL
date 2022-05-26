04de960b28af817cb1e9f0cbbe17f6832e1f6771
Sou-ly <souleyman.boudouh@epfl.ch>
2022-05-26 10:58:19 +0200

    Remove unused imports

--- app/src/main/java/com/github/sdp/ratemyepfl/viewmodel/ClassroomListViewModel.kt
+++ app/src/main/java/com/github/sdp/ratemyepfl/viewmodel/ClassroomListViewModel.kt
@@ -1,13 +1,8 @@
 package com.github.sdp.ratemyepfl.viewmodel
 
-import androidx.lifecycle.MutableLiveData
-import com.github.sdp.ratemyepfl.database.query.OrderDirection
-import com.github.sdp.ratemyepfl.database.reviewable.ClassroomRepository
 import com.github.sdp.ratemyepfl.database.reviewable.ClassroomRepositoryImpl
-import com.github.sdp.ratemyepfl.database.reviewable.ReviewableRepository
 import com.github.sdp.ratemyepfl.model.items.Classroom
 import com.github.sdp.ratemyepfl.viewmodel.filter.ClassroomFilter
-import com.github.sdp.ratemyepfl.viewmodel.filter.ReviewableFilter
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
